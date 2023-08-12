d20b3d6a7da6de79f377413a6fb87e5820b70d01
Byte <deadcardinal293@gmail.com>
2023-08-12 19:22:05 +0700

    Добавлен основные папки

---
+++ market_scraper/paths.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from os import makedirs
+
+ROOT      = Path(__file__).resolve().parent
+LOG_DIR   = ROOT / "logs"
+OUT_DIR   = ROOT.parent / "output"
+
+CONFIG_PATH = ROOT.parent / "config.yaml"
+
+makedirs(LOG_DIR,   exist_ok=True)
+makedirs(OUT_DIR,   exist_ok=True)
