9ae6b7f1af2a9bdb4e360e8b1e12d8a965fbe3d9
Byte <deadcardinal293@gmail.com>
2023-08-12 22:54:59 +0700

    Добавлен основные папки [anibis]

--- market_scraper/paths.py
+++ market_scraper/paths.py
@@ -1,11 +1,17 @@
 from pathlib import Path
 from os import makedirs
 
-ROOT      = Path(__file__).resolve().parent
-LOG_DIR   = ROOT / "logs"
-OUT_DIR   = ROOT.parent / "output"
+ROOT        = Path(__file__).resolve().parent
+LOG_DIR     = ROOT / "logs"
+JSON_DIR    = ROOT / "json"
+OUT_DIR     = ROOT.parent / "output"
+
+ANIBIS_DIR  = JSON_DIR / "anibis"
 
 CONFIG_PATH = ROOT.parent / "config.yaml"
 
-makedirs(LOG_DIR,   exist_ok=True)
-makedirs(OUT_DIR,   exist_ok=True)
+makedirs(LOG_DIR,    exist_ok=True)
+makedirs(OUT_DIR,    exist_ok=True)
+
+makedirs(JSON_DIR,   exist_ok=True)
+makedirs(ANIBIS_DIR, exist_ok=True)
