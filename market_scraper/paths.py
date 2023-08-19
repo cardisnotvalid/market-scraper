4f1898d1d281b037d351c4e9cf28cddb205d2402
Byte <deadcardinal293@gmail.com>
2023-08-20 04:40:02 +0700

    Изменен основные папки

--- market_scraper/paths.py
+++ market_scraper/paths.py
@@ -3,15 +3,11 @@ from os import makedirs
 
 ROOT        = Path(__file__).resolve().parent
 LOG_DIR     = ROOT / "logs"
-JSON_DIR    = ROOT / "json"
 OUT_DIR     = ROOT.parent / "output"
-
-ANIBIS_DIR  = JSON_DIR / "anibis"
+ANIBIS_DIR  = OUT_DIR / "anibis"
 
 CONFIG_PATH = ROOT.parent / "config.yaml"
 
 makedirs(LOG_DIR,    exist_ok=True)
 makedirs(OUT_DIR,    exist_ok=True)
-
-makedirs(JSON_DIR,   exist_ok=True)
 makedirs(ANIBIS_DIR, exist_ok=True)
